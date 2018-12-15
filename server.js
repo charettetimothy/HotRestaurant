a73a4f97a90f1ffdc6fe61f4c8adf2c85c3be790
natedogg484 <nathangschmidt@gmail.com>
2018-12-15 13:20:01 -0500

    testing time

--- server.js
+++ server.js
@@ -30,20 +30,20 @@ var waitList = [
 ]
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
+    res.sendFile(path.join(__dirname, "public/home.html"));
   });
   
-  app.get("/forms", function(req, res) {
-    res.sendFile(path.join(__dirname, "forms.html"));
+  app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/reserve.html"));
   });
 
-  app.get("/home", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
+  app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/tables.html"));
   });
 
-  app.get("/view", function(req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
-  });
+  // app.get("/view", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "view.html"));
+  // });
   
   // Displays reservations
   app.get("/api/reservations", function(req, res) {
@@ -77,6 +77,41 @@ app.post("/api/reservations", function(req, res) {
     console.log(newreservation);
   
     reservations.push(newreservation);
+  
+    res.json(newreservation);
+  });
+
+  app.get("/api/waitList", function(req, res) {
+    return res.json(waitList);
+  });
+  
+  app.get("/api/waitList/:waitList", function(req, res) {
+    var chosen = req.params.waitList;
+  
+    console.log(chosen);
+  
+    for (var i = 0; i < waitList.length; i++) {
+      if (chosen === waitList[i].routeName) {
+        return res.json(waitList[i]);
+      }
+    }
+  
+    return res.json(false);
+  });
+
+// Create New Characters - takes in JSON input
+app.post("/api/waitList", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newreservation = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newreservation.routeName = newreservation.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newreservation);
+  
+    waitList.push(newreservation);
   
     res.json(newreservation);
   });
